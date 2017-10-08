3c36d69cc7691a63a1de5d7dd678904834f1460c
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-08 17:52:38 +0200

    Add files via upload

---
+++ ZRozw1_14A/ZRozw1_14A/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZRozw1_14A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj symbol temperatury, K-Kelvin, C-Celsjusz, F-Fahrenheit");
+            int numerZnaku = Console.Read();
+            char znak = (char)numerZnaku;
+            Console.ReadLine();
+            Console.WriteLine("Podaj wartość temperatury");
+            double temp = Convert.ToDouble(Console.ReadLine());
+            if (znak == 'K')
+            {
+                Console.WriteLine("Temperatura w C = {0}", temp - 273.15);
+                Console.WriteLine("Temperatura w F = {0}", 9*(temp - 273.15)/5+32);
+            }
+            else if (znak == 'C')
+            {
+                Console.WriteLine("Temperatura w K = {0}", temp + 273.15);
+                Console.WriteLine("Temperatura w F = {0}", 9 * temp / 5 + 32);
+            }
+            else if (znak == 'F')
+            {
+                Console.WriteLine("Temperatura w K = {0}", 5 * (temp - 32) / 9 + 273.15);
+                Console.WriteLine("Temperatura w C = {0}", 5 *(temp -32)/ 9);
+            }
+
+            Console.ReadKey();
+          
+        }
+    }
+}
